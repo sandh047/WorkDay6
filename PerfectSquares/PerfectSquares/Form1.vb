b29ab89ac50f913d21494b0e357312e16a57903f
sandh047 <wsandholm@yahoo.com>
2013-10-09 17:11:09 -0500

    Finished

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,17 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim square As Integer = 0
+        Dim count As Integer = 1
+
+        While count < 11
+
+            square = Math.Pow(count, 2)
+            lstResult.Items.Add(CStr((count) & ") " & count & " squared = " & square))
+            count += 1
+
+        End While
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,17 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim pop As Double = 7000000000
+        Dim year As Integer = 2012
+
+        While pop > 6000000
+
+            year -= 1
+            pop = (pop - (pop * 0.013979))      'Takes yearly subtractions based on 1.013979 to the power of 50 years (doubling the Population)
+
+        End While
+
+        MessageBox.Show("It was approximately around " & year & " when there were less than 6 million people on the Earth")
 
     End Sub
 End Class
